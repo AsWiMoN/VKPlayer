d9bd589872461a86d9393305ba6b068269731c6d
srvr-2 <srvr-2@mtk.local>
2018-01-23 16:33:12 +0300

    Initial commit

---
+++ VkPlayer/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace VkPlayer
+{    
+    public partial class FormMain : Form
+    {        
+        public List<Audio> audioList;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            new FormAuthorization().Show();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        public class Audio
+        {
+            public int aid { get; set; }
+            public int owner_id { get; set; }
+            public string artist { get; set; }
+            public string title { get; set; }
+            public int duration { get; set; }
+            public string url { get; set; }
+            public string lurics_id { get; set; }
+            public int genre { get; set; }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!Settings1.Default.auth)
+            {
+                Thread.Sleep(500);
+            }
+            WebRequest request = WebRequest.Create("https://api.vk.com/method/audio.get?owner_id=" + Settings1.Default.id+"&need_user=0&acces_token"+Settings1.Default.token);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responceFromServer = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+            responceFromServer = HttpUtility.HtmlDecode(responceFromServer);
+
+            JToken token = JToken.Parse(responceFromServer);
+            audioList = token["resonce"].Children().Skip(1).Select(c => c.ToObject<Audio>()).ToList();
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                for (int i = 0; i < audioList.Count(); i++)
+                {
+                    listBox1.Items.Add(audioList[i].artist + " - " + audioList[i].title);
+                }
+            });
+        }
+    }
+}
---
+++ VkPlayer/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VkPlayer
+{
+    public partial class FormAuthorization : Form
+    {
+        public FormAuthorization()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAuthorization_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=4962552&scope=audio&display=popup&redirect_uri=https://oauth.vk.com/blank.html&v=5.71&response_type=token");
+        }
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            zagruzkaLabel.Text = "Загрузка";
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            zagruzkaLabel.Text = "Загружено";
+            try
+            {
+                string url = webBrowser1.Url.ToString();
+                string tokenMas = url.Split('#')[1];
+                if (tokenMas[0] == 'a')
+                {
+                    Settings1.Default.token = tokenMas.Split('&')[0].Split('=')[1];
+                    Settings1.Default.id = tokenMas.Split('=')[3];
+                    Settings1.Default.auth = true;
+                    MessageBox.Show("Token=" + Settings1.Default.token + "id=" + Settings1.Default.id, "Auth");
+                    this.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Чет пошло не так...", "Хм...");
+            }
+        }
+    }
+}
